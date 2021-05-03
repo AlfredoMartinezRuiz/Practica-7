925071dec85e0ac549fd0d0cc4ebe5129c5b9186
Aurora Méndez <69736664+auroramc19@users.noreply.github.com>
2021-05-03 18:56:36 -0500

    Update Definiciones.h

--- Definiciones.h
+++ Definiciones.h
@@ -7,18 +7,18 @@
 # include <stdlib.h>
 /* Declaracion de las vars principales */
 WINDOW *juego; 
-int posX, posY; // Variables para controlar al humano
+int posX, posY; // Variables para controlar al monito
 
 /* Declaracion de las funciones principales*/
 void pintarFondo(); // Función que se va encargar de generar el fondo
-void mover(float x, float y);
-void dibujarBloque(float x, float y);
-void moverBloques();
-int comprobarCoindicencia(float x, float y);
-void tiempo();
-int menu();
+void mover(float x, float y); //se encarga de que el monito se mueva
+void dibujarBloque(float x, float y); // genera los bloques 
+void moverBloques(); //se encarga de que los bloques se muevan 
+int comprobarCoindicencia(float x, float y); //determina si el bloque tocó al monito
+void tiempo(); //contador de cada partida
+int menu(); 
 void salir();
-void dibujarHumano(float x, float y);
+void dibujarHumano(float x, float y); //genera al monito
 void escucharTeclas();
 
 # endif
